a944d6199f7e2a2cef247ef236cfc9e78f3299c2
Faysal Shuvo <32492440+FaysalShuvo@users.noreply.github.com>
2021-05-31 00:50:32 +0600

    finished setting up redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Header from "./containers/Header";
+import ProductDetail from "./containers/ProductDetail";
+import ProductLists from "./containers/ProductLists";
 
 function App() {
   return (
     <div className="App">
-      <h1>Shopping Using Redux</h1>
+      <Router>
+        <h1>Shopping Using Redux</h1>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductLists} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
